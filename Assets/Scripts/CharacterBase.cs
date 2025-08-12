5a2cb5a9de269b5e60aab7d4c71ae53e2a45f11d
jrtay <jrtay04@gmail.com>
2025-08-12 17:02:54 +1000

    Dialog System

---
+++ Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    public bool isPlayer = false;
+
+    public Sprite charPortrait;
+    public string charName = "npc";
+    public Color textColour = Color.black;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
