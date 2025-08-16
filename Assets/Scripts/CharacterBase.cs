0a2f73d47d62324e473963e77a0cd8eca1ff744f
jrtay <jrtay04@gmail.com>
2025-08-16 19:39:17 +1000

    Update CharacterBase.cs

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -7,16 +7,4 @@ public class CharacterBase : MonoBehaviour
     public Sprite charPortrait;
     public string charName = "npc";
     public Color textColour = Color.black;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
